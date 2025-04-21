3ab420de4b2baced02cbc4c8bdf898f8bf2ff53f
Arda Timuçin Acar <77367113+Timujaponya@users.noreply.github.com>
2025-04-21 11:49:23 +0300

    Add files via upload

---
+++ src/main/java/com/sau/bm/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.sau.bm.repository;
+
+import com.sau.bm.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
